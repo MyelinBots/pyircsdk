07b5070156ff503fdaa39d097b15c5e3baeb69d5
James <james.trotter@tempmee.com>
2024-05-11 21:35:31 -0400

    feat: allow reconnections and exists on failure (#4)

--- examples/example.py
+++ examples/example.py
@@ -5,12 +5,12 @@ from modules.quit.quit import QuitModule
 from modules.urlparse.urlparse import URLParse
 from pyircsdk import IRCSDKConfig, IRCSDK, Module
 
-irc = IRCSDK(IRCSDKConfig('irc.rizon.net',
-                          6667,
-                          'pyircsdk',
-                          '#toolbot',
-                          'pyircsdk',
-                          'pyircsdk made by nyankochan'
+irc = IRCSDK(IRCSDKConfig(host='irc.rizon.net',
+                          port=6667,
+                          nick='pyircsdk2',
+                          channel='#toolbot',
+                          user='pyircsdk',
+                          realname='pyircsdk made by nyankochan'
                           ))
 
 helloModule = HelloModule(irc)
