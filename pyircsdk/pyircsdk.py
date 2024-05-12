e90af5a90ff980f71d8f2d17941dbcd7c54efffd
James Trotter <james.trotter@tempmee.com>
2024-05-12 08:10:23 -0400

    no connection and no data config

--- pyircsdk/pyircsdk.py
+++ pyircsdk/pyircsdk.py
@@ -20,6 +20,8 @@ class IRCSDKConfig:
     ssl: bool
     nickservFormat: str
     nickservPassword: str
+    nodataTimeout: int
+    connectionTimeout: int
 
     def __init__(self,  **kwargs):
         for k in self.__dataclass_fields__:
@@ -81,7 +83,7 @@ class IRCSDK:
 
     def try_connect(self, retries, wait_secs):
         for attempt in range(retries):
-            self.irc.settimeout(10)  # Set a timeout for the connection attempt
+            self.irc.settimeout(self.config.connectionTimeout or 10)  # Set a timeout for the connection attempt
 
             try:
                 print(f"Attempt {attempt + 1} of {retries}")
@@ -114,10 +116,11 @@ class IRCSDK:
                 else:
                     print("Maximum retry attempts reached, connection failed.")
                     raise
+
     def startRecv(self) -> None:
         while 1:
-            # 2 minutes timeout
-            ready = select.select([self.irc], [], [], 120)
+            # nodataTimeout or 120 seconds
+            ready = select.select([self.irc], [], [], self.config.nodataTimeout or 120)
             if ready[0]:
                 try:
                     data = self.irc.recv(2048)
@@ -132,7 +135,7 @@ class IRCSDK:
                     self.irc.close()
                     exit(1)
             else:
-                print("No data received for 120 seconds, quiting...")
+                print("No data received for %s seconds, quiting..." % str(self.config.nodataTimeout or 120))
                 # force program to close socket and to exit
                 self.irc.close()
                 exit(1)
